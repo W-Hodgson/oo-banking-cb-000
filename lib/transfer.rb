ca3a33c2c19c29950ae4a0b0e454a7387f396b69
Will Hodgson <will.hodgson@current-rms.com>
2019-08-19 08:13:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :status, :amoutn
+  attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
     @sender = sender
