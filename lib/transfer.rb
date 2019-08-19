d66d6d41bd30808b0de054f2cb9ebb7b27fccc33
Will Hodgson <will.hodgson@current-rms.com>
2019-08-19 07:59:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status
+
+  def initialize(sender, receiver)
+    @sender = sender
+    @receriver = receiver
+    @status = "pending"
+  end
 end
