06febf75259bc61f1eaa8b9e36ebb0bfaa8122a1
Will Hodgson <will.hodgson@current-rms.com>
2019-08-19 08:17:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
 
-  def execute_transfer
+  def execute_transaction
     @sender.balance -= amount
     @receiver.balance += amount
   end
